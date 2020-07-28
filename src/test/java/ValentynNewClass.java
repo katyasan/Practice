707f9eb5063ee83afdaaa06ccd8f6afe60f0d61d
Valentyn <valentyn.atroshchenko@gmail.com>
2020-07-28 16:08:30 -0500

    Valentyn's third try

--- src/test/java/ValentynNewClass.java
+++ src/test/java/ValentynNewClass.java
@@ -2,5 +2,6 @@ public class ValentynNewClass {
     public static void main(String[] args) {
         System.out.println("LOL");
         System.out.println("New changes");
+        System.out.println("Third try");
     }
 }
