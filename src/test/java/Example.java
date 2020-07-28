ed9520bf443f9fbc75fe4e6c8d4bbca5a978ab26
Anvar Sono <kerimbekovich89@gmail.com>
2020-07-28 16:34:57 -0500

    Anvar's changes

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -2,5 +2,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("What's up");
+        System.out.println("Anvar's changes");
     }
 }
