98c563283816923fd6e0801d9f6a7ee531dfb713
Anvar Sono <kerimbekovich89@gmail.com>
2020-07-28 16:41:50 -0500

    new commit

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,7 +1,7 @@
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.println("What's up");
         System.out.println("Anvar's changes");
+
     }
 }
