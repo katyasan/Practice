3ed3c600ed5358ada5d8ca42989f22dcee8d41c4
Anvar Sono <kerimbekovich89@gmail.com>
2020-07-28 17:03:00 -0500

    y

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,6 +1,5 @@
 public class Example {
-    public static void main(String[] args) {
-        System.out.println("What's up");
+    public static void main(String[] args) { System.out.println("What's up");
         System.out.println("Anvar's changes");
         System.out.println("Hello World");
         System.out.println("Lets make a conflict");
