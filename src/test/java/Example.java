c1d2545445468c240943c7f4e393fc1482735cc6
Valentyn <valentyn.atroshchenko@gmail.com>
2020-07-28 16:31:11 -0500

    Valentyn's Example.class changes

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,5 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("LOOOOOOOOOOLL");
     }
+
 }
